50c1b40ce051dd13abda2e83a050d69fd2f55f36
AlexHorodnic <alexhorodnic@protonmail.com>
2023-03-29 00:02:09 +0300

    create meta description for the site, edit title based on which page is active.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import {Component} from '@angular/core';
-
+import { Component } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
+interface RouteToTitleMap {
+  [key: string]: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'alexhorodnic.com';
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.updatePageTitle();
+      }
+    });
+  }
+
+  routeToTitle: RouteToTitleMap = {
+    '': 'Home',
+    'projects': 'Projects',
+    'about': 'About',
+    '**': 'Page Not Found'
+  };
+  updatePageTitle() {
+    const currentRoute = window.location.pathname.split('/')[1];
+    const pageTitle = this.routeToTitle[currentRoute] || 'Page Not Found';
+    document.title = `Alex Horodnic - ${pageTitle}`;
+  }
 }
