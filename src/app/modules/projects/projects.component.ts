8ac0f5f8e6f8f36d98758773a67e9819cee62514
AlexHorodnic <alexhorodnic@protonmail.com>
2023-11-16 18:31:38 +0200

    add a new project.

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface PeriodicElement {
   name: string;
@@ -12,39 +12,44 @@ export interface PeriodicElement {
 
 const ELEMENT_DATA: PeriodicElement[] = [
   {
-    name: 'WeatherHub - Show Weather statistics by fetching data from a public API',
-    languages: 'HTML, CSS, BOOTSTRAP, JAVASCRIPT, API',
+    name: 'Employee Management - Simple Fullstack CRUD Application',
+    languages: 'HTML, CSS, BOOTSTRAP, JS, JAVA, MYSQL, DATABASE',
+    description: 'Project Description',
+    img: 'assets/projects/employee-management.png',
+    features: 'Photo and Video preview | Contact Form | Subscription Method',
+    demo: 'https://employee-management-system.alexhorodnic.com/',
+    code: 'https://github.com/AlexHorodnic/employee-mngmt-fullstack',
+  },
+  {
+    name: 'WeatherHub - Show Weather statistics by fetching public API data',
+    languages: 'HTML, CSS, BOOTSTRAP, JS, API',
     description: 'Project Description',
     img: 'assets/projects/Weatherhub.png',
-    features: 'Responsive Design | Contact Form | Sign Up with Paid Subscription',
+    features:
+      'Responsive Design | Contact Form | Sign Up with Paid Subscription',
     demo: 'https://weatherhub.alexhorodnic.com/',
-    code: 'https://github.com/AlexHorodnic/weatherhub'
+    code: 'https://github.com/AlexHorodnic/weatherhub',
   },
   {
     name: 'CarBrandCentral -  A responsive single page application',
-    languages: 'HTML, CSS, BOOTSTRAP, JAVASCRIPT',
+    languages: 'HTML, CSS, BOOTSTRAP, JS',
     description: 'Project Description',
     img: 'assets/projects/CarBrandCentral.jpg',
     features: 'Photo and Video preview | Contact Form | Subscription Method',
     demo: 'https://carbrandcentral.alexhorodnic.com/',
-    code: 'https://github.com/AlexHorodnic/carbrandcentral'
+    code: 'https://github.com/AlexHorodnic/carbrandcentral',
   },
 ];
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
-
   dataSource = ELEMENT_DATA;
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
-
